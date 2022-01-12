e6a304a506b705c47d9a41c0613c06291785074a
Abdulkadir Adamu <adamuhammadabdulkadir@gmail.com>
2022-01-13 00:08:03 +0100

    usign Context api

--- src/App.js
+++ src/App.js
@@ -1,17 +1,54 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { v4 as uuidv4 } from 'uuid';
 import { useState } from 'react';
 import Header from './components/Header';
 import FeedbackList from './components/FeedbackList';
+import FeedbackStats from './components/FeedbackStats';
+import FeedbackForm from './components/FeedbackForm';
+import AboutPage from './pages/AboutPage';
+import { FeedbackProvider } from './context/FeedbackContext';
+
 import FeedbackData from './data/FeedbackData';
+import AboutIconLink from './components/AboutIconLink';
 
 function App() {
-  const [feedBack, setFeedBack] = useState(FeedbackData);
+  const [feedback, setFeedBack] = useState(FeedbackData);
+
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4();
+    setFeedBack([newFeedback, ...feedback]);
+  };
+
+  const deleteFeedback = (id) => {
+    if (window.confirm('Are you sure you want to delete?')) {
+      setFeedBack(feedback.filter((item) => item.id !== id));
+    }
+  };
+
   return (
-    <>
-      <Header />
-      <div className="container">
-        <FeedbackList feedback={feedBack} />
-      </div>
-    </>
+    <FeedbackProvider>
+      <Router>
+        <Header />
+        <div className="container">
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <>
+                  <FeedbackForm handleAdd={addFeedback} />
+                  <FeedbackStats feedback={feedback} />
+                  <FeedbackList handleDelete={deleteFeedback} />
+                </>
+              }
+            />
+
+            <Route path="/about" element={<AboutPage />} />
+          </Routes>
+
+          <AboutIconLink />
+        </div>
+      </Router>
+    </FeedbackProvider>
   );
 }
 
--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,17 +1,43 @@
+import { motion, AnimatePresence } from 'framer-motion';
+import { useContext } from 'react';
+import FeedbackContext from '../context/FeedbackContext';
 import FeedbackItem from '../components/FeedbackItem';
 
-function FeedbackList({ feedback }) {
+function FeedbackList({ handleDelete }) {
+  const { feedback } = useContext(FeedbackContext);
+
   if (!feedback || feedback.length === 0) {
     return <p>No feedback yet!</p>;
   }
 
   return (
     <div className="feedback-list">
-      {feedback.map((item) => (
-        <FeedbackItem key={item.id} item={item} />
-      ))}
+      <AnimatePresence>
+        {feedback.map((item) => (
+          <motion.div
+            key={item.id}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <FeedbackItem
+              handleDelete={handleDelete}
+              key={item.id}
+              item={item}
+            />
+          </motion.div>
+        ))}
+      </AnimatePresence>
     </div>
   );
+
+  // return (
+  //   <div className="feedback-list">
+  //     {feedback.map((item) => (
+  //       <FeedbackItem handleDelete={handleDelete} key={item.id} item={item} />
+  //     ))}
+  //   </div>
+  // );
 }
 
 export default FeedbackList;
