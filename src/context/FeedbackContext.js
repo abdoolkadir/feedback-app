824ca215e2a61e6ecafe93d8792040c8c3c04a18
Abdulkadir Adamu <adamuhammadabdulkadir@gmail.com>
2022-05-02 01:22:10 +0100

    GET, POST, PUT & DELETE req from json server

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,5 +1,4 @@
 import { createContext, useState, useEffect } from 'react';
-import { v4 as uuidv4 } from 'uuid';
 
 const FeedbackContext = createContext();
 
@@ -18,9 +17,7 @@ export const FeedbackProvider = ({ children }) => {
 
   // Fetch feedback
   const fetchFeedback = async () => {
-    const response = await fetch(
-      'http://localhost:5000/feedback?_sort=id&_order=desc'
-    );
+    const response = await fetch(`/feedback?_sort=id&_order=desc`);
 
     const data = await response.json();
 
@@ -29,22 +26,42 @@ export const FeedbackProvider = ({ children }) => {
   };
 
   // Delete feedback
-  const deleteFeedback = (id) => {
+  const deleteFeedback = async (id) => {
     if (window.confirm('Are you sure you want to delete?')) {
+      await fetch(`/feedback/${id}`, { method: 'DELETE' });
       setFeedBack(feedback.filter((item) => item.id !== id));
     }
   };
 
   // Add Feedback
-  const addFeedBack = (newFeedback) => {
-    newFeedback.id = uuidv4();
-    setFeedBack([newFeedback, ...feedback]);
+  const addFeedBack = async (newFeedback) => {
+    const response = await fetch('/feedback', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newFeedback)
+    });
+
+    const data = await response.json();
+
+    setFeedBack([data, ...feedback]);
   };
 
   // Update feedback
-  const updateFeedback = (id, updItem) => {
+  const updateFeedback = async (id, updItem) => {
+    const response = await fetch(`/feedback/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(updItem)
+    });
+
+    const data = await response.json();
+
     setFeedBack(
-      feedback.map((item) => (item.id === id ? { ...item, ...updItem } : item))
+      feedback.map((item) => (item.id === id ? { ...item, ...data } : item))
     );
   };
 
